1efb84dc741a5bafe461329f55c4201689ff5f1e
puresy <puresy.yu@gmail.com>
2026-09-09 21:02:53 +0800

    Deploy 科普之光看板 2026-09-09 21:02

---
+++ kepu-light-board/broadcast.js
@@ -0,0 +1,24 @@
+window.BROADCAST_DATA = {
+  updatedAt: "2026-09-09",
+  asOf: "截至 9月9日 14:00",
+  daily: [
+    { d: "9月1日", views: 1876.5 },
+    { d: "9月2日", views: 1027.1 },
+    { d: "9月3日", views: 1230.6 },
+    { d: "9月4日", views: 777.2 },
+    { d: "9月5日", views: 1522.5 },
+    { d: "9月6日", views: 928.1 },
+    { d: "9月7日", views: 1070.2 },
+    { d: "9月8日", views: 1633.6 }
+  ],
+  topics: [
+    { name: "#科普之光#", views: 9301.9, display: "9301.9万" },
+    { name: "#全国科普月#", views: 20000, display: "2亿" },
+    { name: "#千万IP创科普#", views: 5532.1, display: "5532.1万" }
+  ],
+  totals: {
+    live: "1亿+",
+    topic: "3.48亿",
+    media: "4.48亿"
+  }
+};
